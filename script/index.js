21908e077b85bb014d589dcbe3bcc6796f52b15f
ebiet <ebieter024@gmail.com>
2025-01-29 21:33:23 +0800

    update

--- script/index.js
+++ script/index.js
@@ -24,12 +24,16 @@ button.addEventListener('click',() => {
     if(height_status && weight_status){
     const bmi = (weight / ((height*height)/10000)).toFixed(2);
 
-    if(bmi < 18.6){
-        result.innerHTML = 'kekurangan berat : '+ bmi; }
-    else if(bmi >= 18.6 && bmi <24.9){
+    if(bmi <= 18.5){
+        result.innerHTML = 'berat badan kurang : '+ bmi; }
+    else if(bmi > 18.5 && bmi <23){
         result.innerHTML = 'Normal : '+ bmi;}
+    else if(bmi >= 23 && bmi < 25){
+        result.innerHTML = 'kelebihan berat badan : '+ bmi;}
+    else if(bmi >= 25 && bmi <29.9){
+        result.innerHTML = 'obesitas 1 : '+ bmi;}
     else{
-        result.innerHTML = 'kelebihan berat : '+ bmi;}
+        result.innerHTML = 'obesitas 2 : '+ bmi;}
     }
 
     // if(height_status && weight_status){
